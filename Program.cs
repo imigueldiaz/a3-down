3c04257ebaf336669f5935126eda78b80f01ecbb
imigueldiaz <imigueldiaz@gmail.com>
2018-11-24 13:24:01 +0100

    First version doing something :)

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace a3_down
+{
+
+    class Program
+    {
+        private const string usageText = "Usage: a3-down url";
+        private const string urlNotFound = "URL not found";
+
+        private const string urlNotSupported = "URL not supported by now :(";
+
+
+        private const string OBJNAME = "__PRELOADED_STATE__";
+
+        static void Main(string[] args)
+        {
+
+            var seoTitle = string.Empty;
+            var episodeNumber = string.Empty;
+            var apiJson = string.Empty;
+
+            if (args == null || args.Length == 0)
+            {
+
+                Console.WriteLine(usageText);
+                return;
+            }
+
+            string url = args[0];
+
+            var doc = new HtmlAgilityPack.HtmlWeb();
+
+            var htmlDoc = doc.Load(url);
+
+            if (htmlDoc == null)
+            {
+                Console.WriteLine(urlNotFound);
+                return;
+            }
+
+            Console.WriteLine("HTML document adquired, parsing...");
+
+            var script = htmlDoc.DocumentNode.Descendants().Where(d => d.Name == "script" && d.InnerText.Contains(OBJNAME)).FirstOrDefault();
+
+            if (script == null)
+            {
+                Console.WriteLine(urlNotSupported);
+                return;
+
+            }
+
+            var obj = cleanScript(script);
+
+            dynamic data = JsonConvert.DeserializeObject(obj);
+
+            if (data != null)
+            {
+                Console.WriteLine("Document parsed!");
+
+                seoTitle = data.SelectToken("$..episodes..seoTitle");
+                episodeNumber = data.SelectToken("$..episodes..numberOfEpisode");
+                apiJson = data.SelectToken("$..episodes..urlVideo");
+
+                Console.WriteLine($@"Found: {episodeNumber} - {seoTitle}");
+                
+            }
+
+        }
+
+        private static string cleanScript(HtmlNode script)
+        {
+
+            var cleanScript = script.InnerText.Replace("window.", "");
+            var objString = string.Empty;
+
+            foreach (string line in cleanScript.Split('\n', StringSplitOptions.RemoveEmptyEntries))
+            {
+
+                if (line.Trim().StartsWith(OBJNAME))
+                {
+                    objString = line.Trim().Remove(0, OBJNAME.Length).Replace("=", "").Trim();
+                    break;
+                }
+            }
+
+            return objString.Replace(";", "");
+
+        }
+
+    }
+}
